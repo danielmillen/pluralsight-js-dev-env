ab2233f5e8c9398a0eb9566a896db0c95408ca8d
Daniel Millen <daniel.millen@slalom.com>
2017-06-27 16:43:11 -0700

    Update for prod deployment

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'http://nameless-brushlands-87985.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
